13353140689f32923e8aa73b1b803c2ea0816cc9
Adithya Balaji <adithyabsk@gmail.com>
2018-01-01 23:17:25 -0500

    Fix get_stats script errors

--- get_stats.py
+++ get_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-import json
+
 import pandas as pd
-import matplotlib.pyplot as plt
 from nba_py import player
 from datetime import datetime
 from tqdm import tqdm
@@ -11,27 +10,28 @@ now_str = now.strftime('%Y_%m_%d')
 file_name = 'nba_data_{}.h5'.format(now_str)
 
 print('Getting list of active players on {}'.format(now_str))
-player_df = player.PlayerList(season='2017-18').info()
+p_list = player.PlayerList(season='2017-18').info()
 print('Serializing player list...')
-player_df.to_hdf(file_name, 'player_data', mode='w', format='table')
-player_ids = list(player_df['PERSON_ID'])
+p_list.to_hdf(file_name, 'p_list', mode='w', format='table')
+p_ids = list(p_list['PERSON_ID'])
 
 
 print('Getting logs for every player...')
-player_logs = pd.DataFrame()
-for p_id in tqdm(player_ids):
-	# tqdm.write(player_df.loc[player_df['PERSON_ID'] == p_id, 'DISPLAY_LAST_COMMA_FIRST'].values[0])
-	player_log = player.PlayerGameLogs(p_id, season='2017-18').info()
-	player_log['FANTASY'] = player_log.PTS + \
-							 player_log.STL * 3 + \
-							 player_log.REB * 1.2 + \
-							 player_log.FGM + \
-							 player_log.FTM + \
-							 player_log.BLK * 3 + \
-							 player_log.AST * 1.5 + \
-							 player_log.TOV * -1 + \
-							 player_log.FGA * -1 + \
-							 player_log.FTA * -1
-	player_logs = player_logs.append(player_log, ignore_index=True)
+p_logs = pd.DataFrame()
+for p_id in tqdm(p_ids):
+	# tqdm.write(p_list.loc[p_list['PERSON_ID'] == p_id, 'DISPLAY_LAST_COMMA_FIRST'].values[0])
+	p_logs = p_logs.append(player.PlayerGameLogs(p_id, season='2017-18').info(), ignore_index=True)
+print('Computing Fantasy Score...')
+p_logs[p_logs.columns.difference(['GAME_DATE', 'MATCHUP', 'WL'])] = p_logs[p_logs.columns.difference(['GAME_DATE', 'MATCHUP', 'WL'])].apply(pd.to_numeric)
+p_logs['FANTASY'] = p_logs.PTS + \
+						  	p_logs.STL * 3 + \
+						  	p_logs.REB * 1.2 + \
+						  	p_logs.FGM + \
+						  	p_logs.FTM + \
+						  	p_logs.BLK * 3 + \
+						  	p_logs.AST * 1.5 + \
+						  	p_logs.TOV * -1 + \
+						  	p_logs.FGA * -1 + \
+						  	p_logs.FTA * -1
 print('Serializing player logs...')
-player_df.to_hdf(file_name, 'player_logs', mode='a', format='table')
+p_logs.to_hdf(file_name, 'p_logs', mode='a', format='table')
